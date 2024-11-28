6b76c0fc617cb576c07b499765bc1cf00ccf7c33
EqUiNoX <equinox.teamresurgent@gmail.com>
2024-11-28 09:12:40 -0700

    Update deviceEEPROM.cpp

--- PIDisplayLib/src/deviceEEPROM.cpp
+++ PIDisplayLib/src/deviceEEPROM.cpp
@@ -27,9 +27,9 @@
 
 void deviceEEPROM::initI2c(uint32_t address)
 {
-	mI2cAddress = address;
+    mI2cAddress = address;
 
-	i2c_init(DEVICE_EEPROM_I2C, DEVICE_EEPROM_I2C_BAUDRATE);
+    i2c_init(DEVICE_EEPROM_I2C, DEVICE_EEPROM_I2C_BAUDRATE);
     gpio_set_function(DEVICE_EEPROM_I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(DEVICE_EEPROM_I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(DEVICE_EEPROM_I2C_SDA);
@@ -39,21 +39,21 @@ void deviceEEPROM::initI2c(uint32_t address)
 
 void deviceEEPROM::scanI2c()
 {
-   	uint8_t testvalue = 0;
-	for (int32_t address = 0; address < 256; address++)
-	{
+    uint8_t testvalue = 0;
+    for (int32_t address = 0; address < 256; address++)
+    {
         uint8_t reg = 0;
-		if (i2c_write_timeout_us(DEVICE_EEPROM_I2C, address, &reg, 1, false, DEVICE_EEPROM_I2C_TIMEOUT_US) == 1)
-		{
+        if (i2c_write_timeout_us(DEVICE_EEPROM_I2C, address, &reg, 1, false, DEVICE_EEPROM_I2C_TIMEOUT_US) == 1)
+        {
             printf("Found I2C device on address 0x%02x.\n", address);
         }
-	}
+    }
 }
 
 bool deviceEEPROM::read()
 {
     uint8_t reg = DEVICE_EEPROM_DATETIME_REG;
-    
+
     if (i2c_write_timeout_us(DEVICE_EEPROM_I2C, mI2cAddress, &reg, 1, true, DEVICE_EEPROM_I2C_TIMEOUT_US) != 1)
     {
         printf("Fail1\n");
